5c93a94fb9c2bf6ce11b6c640fcc50920bb452e7
Patrick Crisostomo <kylecode7213@gmail.com>
2026-05-21 20:03:55 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,6 @@
     'depends': ['sale', 'stock', 'delivery'],
     'data': [
         'security/ir.model.access.csv',
-        'data/lalamove_data.xml',
         'views/lalamove_config_views.xml',
         'views/sale_order_views.xml',
         'views/stock_picking_views.xml',
